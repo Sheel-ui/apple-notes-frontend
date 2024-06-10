063193b0d2f29c80253255b56e1e115230312ee0
Sheel-ui <sheeltaskar64@gmail.com>
2024-06-10 21:29:34 +0530

    add fetch and populate list data

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,277 +1,87 @@
 import { FaListUl } from "react-icons/fa6";
 import { BsTrash3 } from "react-icons/bs";
 import { FiGrid } from "react-icons/fi";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const List = () => {
-	return (
-		<div className="List bg-accent-500 w-[20%] flex flex-col">
-			<div className="bg-accent-200 flex justify-between p-3 border-b border-black ">
-				<div className="flex">
-					<div className="px-2 py-1 text-gray-200 bg-white bg-opacity-10 rounded">
-						<FaListUl />
-					</div>
-					<div className="px-2 py-1 text-gray-400">
-						<FiGrid />
-					</div>
-				</div>
-				<div className="px-2 py-1 text-gray-400">
-					<BsTrash3 />
-				</div>
-			</div>
+  const [data, setData] = useState([]);
+  const [selectedItemId, setSelectedItemId] = useState(null);
 
-			<div className="text-gray-200 text-[12px] flex flex-1 flex-col overflow-y-auto">
-				<h1 className="absolute px-4 border-b border-accent-200 bg-accent-500 w-[20%] text-gray-400 font-bold py-1 mb-2">
-					Today
-				</h1>
-				<div className="flex-1">
-					<div className="mx-4 mb-4">
-						<h1 className="text-gray-400 font-bold py-1 mb-2">
-							Yesterday
-						</h1>
-						<div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="mx-4">
-						<h1 className="text-gray-400 font-bold py-1 mb-2">
-							Yesterday
-						</h1>
-						<div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-						</div>
-					</div>
-                    <div className="mx-4">
-						<h1 className="text-gray-400 font-bold py-1 mb-2">
-							Yesterday
-						</h1>
-						<div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-						</div>
-					</div>
-                    <div className="mx-4">
-						<h1 className="text-gray-400 font-bold py-1 mb-2">
-							Yesterday
-						</h1>
-						<div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3 border-b border-accent-200">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-							<div className="mx-4 py-3">
-								<h1 className="font-bold text-[13px]">title</h1>
-								<div className="space-x-2">
-									<span>1:23 PM</span>
-									<span className="text-gray-400">
-										description
-									</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = "http://localhost:8080/segregated-todos";
+        const response = await axios.get(url);
+        setData(response.data);
+
+        const firstKey = Object.keys(response.data)[0];
+        if (response.data[firstKey] && response.data[firstKey].length > 0) {
+          setSelectedItemId(response.data[firstKey][0].id);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleItemClick = (id) => {
+    setSelectedItemId(id);
+  };
+
+  return (
+    <div className="List bg-accent-500 w-[20%] flex flex-col">
+      <div className="bg-accent-200 flex justify-between p-3 border-b border-black ">
+        <div className="flex">
+          <div className="px-2 py-1 text-gray-200 bg-white bg-opacity-10 rounded">
+            <FaListUl />
+          </div>
+          <div className="px-2 py-1 text-gray-400">
+            <FiGrid />
+          </div>
+        </div>
+        <div className="px-2 py-1 text-gray-400">
+          <BsTrash3 />
+        </div>
+      </div>
+
+      <div className="text-gray-200 text-[12px] flex flex-1 flex-col overflow-y-auto no-scrollbar">
+        <h1 className="absolute px-4 border-b border-accent-200 bg-accent-500 w-[20%] text-gray-400 font-bold py-1 mb-2">
+          Today
+        </h1>
+        <div className="flex-1">
+          {Object.keys(data).map((key) => (
+            <div key={key} className="mx-4 mb-4">
+              <h1 className="text-gray-400 font-bold py-1 mb-2">
+                {key.charAt(0).toUpperCase() + key.slice(1)}
+              </h1>
+              <div>
+                {data[key].map((item, index) => (
+                  <div
+                    key={item.id}
+                    className={`py-3 px-4 rounded-lg cursor-arrow ${
+                      index !== data[key].length - 1
+                        ? "border-b border-accent-200"
+                        : ""
+                    } ${selectedItemId === item.id ? "bg-accent-50" : ""}`}
+                    onClick={() => handleItemClick(item.id)}
+                  >
+                    <h1 className="font-bold text-[13px]">{item.title}</h1>
+                    <div className="space-x-2 truncate overflow-hidden text-gray-400">
+                      <span className="text-gray-200">{item.createdAt}</span>
+                      <span>{item.description}</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default List;
