e0e7e449c2a4466683c75a9b41303e1970ea9592
Sheel-ui <sheeltaskar64@gmail.com>
2024-06-10 22:51:17 +0530

    item select bug fix

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,34 +4,47 @@ import { FiGrid } from "react-icons/fi";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+const fetchData = async (setData, onUserClick,setSelectedItemId) => {
+	try {
+	  const url = "http://localhost:8080/segregated-todos";
+	  const response = await axios.get(url);
+	  setData(response.data);
+  
+	  const firstKey = Object.keys(response.data)[0];
+	  if (response.data[firstKey] && response.data[firstKey].length > 0) {
+		setSelectedItemId(response.data[firstKey][0].id);
+		onUserClick(response.data[firstKey][0].id);
+	  }
+	} catch (error) {
+	  console.error("Error fetching data:", error);
+	}
+  };
+
 const List = ({ onUserClick }) => {
   const [data, setData] = useState([]);
   const [selectedItemId, setSelectedItemId] = useState(null);
+  const [deletedItem, setDeletedItem] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const url = "http://localhost:8080/segregated-todos";
-        const response = await axios.get(url);
-        setData(response.data);
+    fetchData(setData, onUserClick,setSelectedItemId);
+  }, [onUserClick, deletedItem]);
 
-        const firstKey = Object.keys(response.data)[0];
-        if (response.data[firstKey] && response.data[firstKey].length > 0) {
-          setSelectedItemId(response.data[firstKey][0].id);
-		  onUserClick(response.data[firstKey][0].id);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, [onUserClick]);
+  const handleDelete = async (id) => {
+    try {
+      const url = `http://localhost:8080/item/${id}`;
+      await axios.delete(url);
+      setDeletedItem(id);
+	  setSelectedItemId(-1);
+    } catch (error) {
+      console.error("Error deleting item:", error);
+    }
+  };
 
   const handleItemClick = (id) => {
 	onUserClick(id);
     setSelectedItemId(id);
   };
+
 
   return (
     <div className="List bg-accent-500 w-[20%] flex flex-col">
@@ -44,7 +57,9 @@ const List = ({ onUserClick }) => {
             <FiGrid />
           </div>
         </div>
-        <div className="px-2 py-1 text-gray-400">
+        <div className="px-2 py-1 text-gray-400 "
+		onClick={() => handleDelete(selectedItemId)}
+		style={{ cursor: 'pointer' }}>
           <BsTrash3 />
         </div>
       </div>
